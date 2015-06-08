da8788ea10f0d2f385b4490b1dc53f321fa473d8
Michael Herman <hermanmu@gmail.com>
2015-06-08 08:32:19 -0600

    part 2

--- tests/test_config.py
+++ tests/test_config.py
@@ -16,7 +16,6 @@ class TestDevelopmentConfig(TestCase):
         return app
 
     def test_app_is_development(self):
-        self.assertFalse(current_app.config['TESTING'])
         self.assertTrue(app.config['DEBUG'] is True)
         self.assertTrue(app.config['WTF_CSRF_ENABLED'] is False)
         self.assertTrue(app.config['DEBUG_TB_ENABLED'] is True)
@@ -42,7 +41,6 @@ class TestProductionConfig(TestCase):
         return app
 
     def test_app_is_production(self):
-        self.assertFalse(current_app.config['TESTING'])
         self.assertTrue(app.config['DEBUG'] is False)
         self.assertTrue(app.config['DEBUG_TB_ENABLED'] is False)
         self.assertTrue(app.config['WTF_CSRF_ENABLED'] is True)
